29d758bb898f00cedaa3427b6b175dcab31e0e4a
Heather Hartley <hlhartley@gmail.com>
2018-10-25 11:33:57 -0600

    Save to storage and refactoring

--- idea.js
+++ idea.js
@@ -1,36 +1,17 @@
 class Idea {
   constructor(title, body) {
+    this.id = Math.floor(Date.now() / 1000); 
     this.title = title;
     this.body = body;
+    this.quality = 'swill';
     console.log(this.body);
     console.log(this.title);
   }
 
-  displayCard() {
-    var uniqueCardId = Math.floor(Date.now() / 1000);
-    var cardsContainer = document.querySelector('.cards-container');
-    var card = `<article id="${uniqueCardId}" class="idea-card">
-      <section class="output-container">
-        <h1 class="title-output" contenteditable="true">${this.title}</h1> 
-        <p class="body-output" contenteditable="true">${this.body}</p>
-      </section>
-      <section class="quality-container">
-        <div class="left-quality-container">
-          <img class="quality-icons downvote-btn" src="images/downvote.svg">
-          <img class="quality-icons upvote-btn" src="images/upvote.svg">
-          <h2 class="quality-header">Quality: Swill</h2>
-        </div>
-        <div class="right-quality-container">
-        <img class="quality-icons delete-btn" src="images/delete.svg">
-        </div>
-      </section>
-    </article>`;
-    cardsContainer.innerHTML = cardsContainer.innerHTML + card;
+ saveToStorage() {
+    console.log(this);
   }
 
-  saveToStorage() {
-    // save info to local storage
-  }
 
   deleteFromStorage() {
     // delete from local storage
